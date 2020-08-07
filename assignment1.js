725cc061de611bb07280c9299cb52dc477fe0685
Shubham <ciruslivingstone@gmail.com>
2020-08-07 20:32:15 +0530

    Missing condition for 'places array' added

--- assignment1.js
+++ assignment1.js
@@ -90,19 +90,25 @@ let places = [
 
 let input = promt("Enter Cafe Name:");
 input = input.toUpperCase();
+console.log(findCafe(input));
 //console.log(input);
-let Result=[];
-for(let i=0;i<cafe.length;i++){
-    if(cafe[i].name.toUpperCase().startsWith(input) === true){
-        // console.log(`MAtched with ${cafe[i].name.toUpperCase()}`);
-        let search_id=cafe[i].place_id;
-        // console.log(search_id);
-        for(let j=0;j<places.length;j++){
-        let new_obj=Object.assign({},places[i]);
-        new_obj["name"]=cafe[i].name;
-        delete new_obj.id;
-        Result.push(new_obj);
+
+function findCafe(inp) {
+  Result = [];
+  for (let i = 0; i < cafe.length; i++) {
+    if (cafe[i].name.toUpperCase().startsWith(inp) === true) {
+      // console.log(`MAtched with ${cafe[i].name.toUpperCase()}`);
+      let search_id = cafe[i].place_id;
+      // console.log(search_id);
+      for (let j = 0; j < places.length; j++) {
+        if (places[j].id === search_id) {
+          let new_obj = Object.assign({}, places[i]);
+          new_obj["name"] = cafe[i].name;
+          delete new_obj.id;
+          Result.push(new_obj);
         }
+      }
     }
-    console.log(Result);
+  }
+  return Result;
 }
